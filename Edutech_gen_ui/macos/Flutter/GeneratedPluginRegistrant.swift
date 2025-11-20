45dbb4941e0cde5176845042768c09725b1f7a3c
tarun-naga-sai <tarunnagasai007@gmail.com>
2025-11-20 12:27:10 +0530

    feat: Add webview, YouTube player, and shimmer_ai packages, update Linux application ID, and register associated plugins.

--- Edutech_gen_ui/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Edutech_gen_ui/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,11 +8,17 @@ import Foundation
 import firebase_app_check
 import firebase_auth
 import firebase_core
+import flutter_inappwebview_macos
 import shared_preferences_foundation
+import url_launcher_macos
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAppCheckPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAppCheckPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
